3267b35381fd07eae202e434fea5e8af96533828
Max <maxh2709@gmail.com>
2022-05-11 21:25:51 +0200

    Update KubeJS dependency, attempt to fix #7

--- src/main/java/dev/latvian/kubejs/mekanism/recipe/MekanismRecipeJS.java
+++ src/main/java/dev/latvian/kubejs/mekanism/recipe/MekanismRecipeJS.java
@@ -1,15 +1,21 @@
 package dev.latvian.kubejs.mekanism.recipe;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import dev.latvian.mods.kubejs.item.ingredient.IngredientJS;
 import dev.latvian.mods.kubejs.item.ingredient.IngredientStackJS;
 import dev.latvian.mods.kubejs.recipe.RecipeJS;
 import dev.latvian.mods.kubejs.util.MapJS;
+import dev.latvian.mods.rhino.mod.util.JsonUtils;
+import mekanism.api.JsonConstants;
 import mekanism.api.SerializerHelper;
+import mekanism.api.chemical.Chemical;
+import mekanism.api.chemical.ChemicalStack;
 import mekanism.api.chemical.gas.GasStack;
 import mekanism.api.chemical.merged.BoxedChemicalStack;
 import mekanism.api.recipes.ingredients.ChemicalStackIngredient;
+import mekanism.api.recipes.ingredients.creator.IChemicalStackIngredientCreator;
 import mekanism.api.recipes.ingredients.creator.IngredientCreatorAccess;
 
 import javax.annotation.Nullable;
@@ -31,22 +37,58 @@ public abstract class MekanismRecipeJS extends RecipeJS {
 	}
 
 	public static ChemicalStackIngredient.GasStackIngredient parseGasIngredient(@Nullable Object o) {
-		if (o instanceof JsonElement json) {
-			return IngredientCreatorAccess.gas().deserialize(json);
-		} else if (o instanceof CharSequence) {
-			JsonObject json = new JsonObject();
-			json.addProperty("gas", o.toString());
-			json.addProperty("amount", 1000);
-			return IngredientCreatorAccess.gas().deserialize(json);
+		return parseChemicalIngredient(o, JsonConstants.GAS, 1000, IngredientCreatorAccess.gas());
+	}
+
+	public static ChemicalStackIngredient.InfusionStackIngredient parseInfusionIngredient(@Nullable Object o) {
+		return parseChemicalIngredient(o, JsonConstants.INFUSE_TYPE, 10, IngredientCreatorAccess.infusion());
+	}
+
+	private static <
+			C extends Chemical<C>,
+			S extends ChemicalStack<C>,
+			I extends ChemicalStackIngredient<C, S>
+			> I parseChemicalIngredient(@Nullable Object o, String jsonKey, int defaultAmount, IChemicalStackIngredientCreator<C, S, I> creator) {
+		// TODO: make a proper wrapper (whenever i feel like it lol)
+		return creator.deserialize(wrapChemical(JsonUtils.of(o), jsonKey, defaultAmount));
+	}
+
+	private static JsonElement wrapChemical(JsonElement json, String jsonKey, int defaultAmount) {
+		if (json.isJsonPrimitive()) {
+			var string = json.getAsString().trim();
+			var amount = defaultAmount;
+
+			int spaceIndex = string.indexOf(' ');
+			if (spaceIndex >= 2 && string.indexOf('x') == spaceIndex - 1) {
+				amount = Integer.parseInt(string.substring(0, spaceIndex - 1));
+				string = string.substring(spaceIndex + 1);
+			}
+
+			var output = new JsonObject();
+
+			if (string.startsWith("#")) {
+				output.addProperty("tag", string.substring(1));
+			} else {
+				output.addProperty(jsonKey, string);
+			}
+
+			output.addProperty("amount", amount);
+			return output;
+		} else if (json.isJsonObject()) {
+			var output = json.getAsJsonObject();
+			if (!output.has("amount")) {
+				output.addProperty("amount", defaultAmount);
+			}
+			return output;
+		} else if (json.isJsonArray()) {
+			var output = new JsonArray();
+			for (var element : json.getAsJsonArray()) {
+				output.add(wrapChemical(element, jsonKey, defaultAmount));
+			}
+			return output;
+		} else {
+			throw new IllegalArgumentException("Invalid chemical ingredient: " + json);
 		}
-
-		JsonObject json = MapJS.of(o).toJson();
-
-		if (!json.has("amount")) {
-			json.addProperty("amount", 1000);
-		}
-
-		return IngredientCreatorAccess.gas().deserialize(json);
 	}
 
 	public static GasStack parseGasResult(@Nullable Object o) {
--- src/main/java/dev/latvian/kubejs/mekanism/recipe/MetallurgicInfusingRecipeJS.java
+++ src/main/java/dev/latvian/kubejs/mekanism/recipe/MetallurgicInfusingRecipeJS.java
@@ -1,60 +1,48 @@
 package dev.latvian.kubejs.mekanism.recipe;
 
-import com.google.gson.JsonObject;
 import dev.latvian.mods.kubejs.util.ListJS;
+import mekanism.api.JsonConstants;
+import mekanism.api.recipes.ingredients.ChemicalStackIngredient;
 
 /**
  * @author LatvianModder
  */
 public class MetallurgicInfusingRecipeJS extends MekanismRecipeJS {
-	public String infusionTag = "mekanism:redstone";
-	public int infusionAmount = 10;
+	private ChemicalStackIngredient.InfusionStackIngredient chemicalInput = parseInfusionIngredient("10x mekanism:redstone");
 
 	@Override
 	public void create(ListJS args) {
 		outputItems.add(parseResultItem(args.get(0)));
 		inputItems.add(parseIngredientItem(args.get(1)).asIngredientStack());
-
 		if (args.size() >= 3) {
-			infusionTag = args.get(2).toString();
-
-			if (args.size() >= 4) {
-				infusionAmount = ((Number) args.get(3)).intValue();
-			}
+			chemicalInput = parseInfusionIngredient(args.get(2));
 		}
+	}
+
+	public MetallurgicInfusingRecipeJS infuseType(Object o) {
+		chemicalInput = parseInfusionIngredient(o);
+		return this;
 	}
 
 	@Override
 	public void deserialize() {
-		outputItems.add(parseResultItem(json.get("output")));
-		inputItems.add(parseIngredientItem(json.get("itemInput")).asIngredientStack());
+		outputItems.add(parseResultItem(json.get(JsonConstants.OUTPUT)));
+		inputItems.add(parseIngredientItem(json.get(JsonConstants.ITEM_INPUT)).asIngredientStack());
 
-		if (json.has("infusionInput")) {
-			JsonObject o = json.get("infusionInput").getAsJsonObject();
-
-			if (o.has("tag")) {
-				infusionTag = o.get("tag").getAsString();
-			}
-
-			if (o.has("amount")) {
-				infusionAmount = o.get("amount").getAsInt();
-			}
+		if (json.has(JsonConstants.CHEMICAL_INPUT)) {
+			chemicalInput = parseInfusionIngredient(json.get(JsonConstants.CHEMICAL_INPUT));
 		}
 	}
 
 	@Override
 	public void serialize() {
 		if (serializeOutputs) {
-			json.add("output", outputItems.get(0).toResultJson());
+			json.add(JsonConstants.OUTPUT, outputItems.get(0).toResultJson());
 		}
 
 		if (serializeInputs) {
-			json.add("itemInput", inputItems.get(0).toJson());
-
-			JsonObject o = new JsonObject();
-			o.addProperty("tag", infusionTag);
-			o.addProperty("amount", infusionAmount);
-			json.add("infusionInput", o);
+			json.add(JsonConstants.ITEM_INPUT, inputItems.get(0).toJson());
+			json.add(JsonConstants.CHEMICAL_INPUT, chemicalInput.serialize());
 		}
 	}
 }
