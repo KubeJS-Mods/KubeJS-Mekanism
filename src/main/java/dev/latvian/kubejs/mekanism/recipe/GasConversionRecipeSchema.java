f567f5fd74c56dc9f29b05899c7e05bfaa8db8e3
Max <maxh2709@gmail.com>
2023-07-17 12:26:53 +0200

    Use MekComponents.INPUT_ITEM instead of ItemComponent.INPUT

--- src/main/java/dev/latvian/kubejs/mekanism/recipe/GasConversionRecipeSchema.java
+++ src/main/java/dev/latvian/kubejs/mekanism/recipe/GasConversionRecipeSchema.java
@@ -1,13 +1,12 @@
 package dev.latvian.kubejs.mekanism.recipe;
 
-import dev.latvian.mods.kubejs.item.InputItem;
 import dev.latvian.mods.kubejs.recipe.RecipeKey;
-import dev.latvian.mods.kubejs.recipe.component.ItemComponents;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
 import mekanism.api.JsonConstants;
+import mekanism.api.recipes.ingredients.ItemStackIngredient;
 
 public interface GasConversionRecipeSchema {
-	RecipeKey<InputItem> INPUT = ItemComponents.INPUT.key(JsonConstants.INPUT);
+	RecipeKey<ItemStackIngredient> INPUT = MekComponents.INPUT_ITEM.key(JsonConstants.INPUT);
 
 	RecipeSchema SCHEMA = new RecipeSchema(MekanismRecipeJS.class, MekanismRecipeJS::new, INPUT);
 }
