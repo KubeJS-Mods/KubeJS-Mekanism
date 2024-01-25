560cf76f454a7c100e5bcc804e37218490b0a917
FrozenGalaxy <156858866+FrozenGalaxy@users.noreply.github.com>
2024-01-25 16:25:26 +0100

    fix some renaming

--- src/main/java/dev/latvian/kubejs/mekanism/recipe/MetallurgicInfusingRecipeSchema.java
+++ src/main/java/dev/latvian/kubejs/mekanism/recipe/MetallurgicInfusingRecipeSchema.java
@@ -1,6 +1,5 @@
 package dev.latvian.kubejs.mekanism.recipe;
 
-import dev.latvian.mods.kubejs.item.InputItem;
 import dev.latvian.mods.kubejs.item.OutputItem;
 import dev.latvian.mods.kubejs.recipe.RecipeKey;
 import dev.latvian.mods.kubejs.recipe.component.ItemComponents;
@@ -13,8 +12,8 @@ import mekanism.common.tags.MekanismTags;
 
 public interface MetallurgicInfusingRecipeSchema {
 	RecipeKey<OutputItem> OUTPUT = ItemComponents.OUTPUT.key(JsonConstants.OUTPUT);
-	RecipeKey<ItemStackIngredient> INPUT = MekComponents.INPUT_ITEM.key(JsonConstants.INPUT);
+	RecipeKey<ItemStackIngredient> ITEM_INPUT = MekComponents.INPUT_ITEM.key(JsonConstants.ITEM_INPUT);
 	RecipeKey<ChemicalStackIngredient.InfusionStackIngredient> CHEMICAL_INPUT = MekComponents.INFUSE_TYPE_INPUT.key(JsonConstants.CHEMICAL_INPUT).optional(IngredientCreatorAccess.infusion().from(MekanismTags.InfuseTypes.REDSTONE, 10));
 
-	RecipeSchema SCHEMA = new RecipeSchema(MekanismRecipeJS.class, MekanismRecipeJS::new, OUTPUT, INPUT, CHEMICAL_INPUT);
+	RecipeSchema SCHEMA = new RecipeSchema(MekanismRecipeJS.class, MekanismRecipeJS::new, OUTPUT, ITEM_INPUT, CHEMICAL_INPUT);
 }
