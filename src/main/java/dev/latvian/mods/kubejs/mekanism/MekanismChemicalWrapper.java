e3f681b58b8dd0ebd309fdebccfeda417bf0a497
max. <max@someone.ky>
2025-11-06 19:28:09 +0100

    Update MekanismChemicalWrapper to new KubeJS builds

--- src/main/java/dev/latvian/mods/kubejs/mekanism/MekanismChemicalWrapper.java
+++ src/main/java/dev/latvian/mods/kubejs/mekanism/MekanismChemicalWrapper.java
@@ -2,9 +2,15 @@ package dev.latvian.mods.kubejs.mekanism;
 
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import dev.latvian.mods.kubejs.fluid.FluidWrapper;
+import com.mojang.serialization.DataResult;
+import dev.latvian.mods.kubejs.error.KubeRuntimeException;
 import dev.latvian.mods.kubejs.holder.HolderWrapper;
 import dev.latvian.mods.kubejs.script.KubeJSContext;
+import dev.latvian.mods.kubejs.script.SourceLine;
+import dev.latvian.mods.kubejs.util.ID;
+import dev.latvian.mods.kubejs.util.RegistryAccessContainer;
+import dev.latvian.mods.rhino.Context;
+import dev.latvian.mods.rhino.Wrapper;
 import dev.latvian.mods.rhino.type.TypeInfo;
 import dev.latvian.mods.rhino.util.HideFromJS;
 import mekanism.api.MekanismAPI;
@@ -21,9 +27,16 @@ import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.neoforged.neoforge.fluids.FluidType;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.mojang.serialization.DataResult.error;
+import static com.mojang.serialization.DataResult.success;
 
 public interface MekanismChemicalWrapper {
 	TypeInfo CHEMICAL_TYPE_INFO = TypeInfo.of(Chemical.class);
@@ -31,6 +44,14 @@ public interface MekanismChemicalWrapper {
 	TypeInfo CHEMICAL_STACK_TYPE_INFO = TypeInfo.of(ChemicalStack.class);
 	TypeInfo CHEMICAL_INGREDIENT_TYPE_INFO = TypeInfo.of(ChemicalIngredient.class);
 	TypeInfo CHEMICAL_STACK_INGREDIENT_TYPE_INFO = TypeInfo.of(ChemicalStackIngredient.class);
+
+	DataResult<ChemicalStack> EMPTY_STACK_RESULT = success(ChemicalStack.EMPTY);
+	DataResult<ChemicalIngredient> EMPTY_INGREDIENT_RESULT = success(EmptyChemicalIngredient.INSTANCE);
+
+	@HideFromJS
+	private static Holder<Chemical> wrapAsHolder(Chemical chem) {
+		return MekanismAPI.CHEMICAL_REGISTRY.wrapAsHolder(chem);
+	}
 
 	static Holder<Chemical> of(KubeJSContext cx, Object from) {
 		return switch (from) {
@@ -56,115 +77,245 @@ public interface MekanismChemicalWrapper {
 		return new ChemicalStackIngredient(ingredient, amount);
 	}
 
-	static ChemicalStack stackOf(Object from) {
-		return switch (from) {
-			case null -> null;
-			case ChemicalStack c -> c;
-			case Chemical c -> new ChemicalStack(c.getAsHolder(), FluidType.BUCKET_VOLUME);
-			case Holder<?> c -> new ChemicalStack((Holder) c, FluidType.BUCKET_VOLUME);
-			default -> {
-				var str = from.toString();
+	static ChemicalStack stackOf(Context cx, Object from) {
+		return tryWrapStack(cx, from)
+			.getOrThrow(error -> new KubeRuntimeException("Failed to read chemical stack from %s: %s".formatted(from, error))
+				.source(SourceLine.of(cx)));
+	}
 
-				if (str.isEmpty() || str.equals("mekanism:empty")) {
-					yield ChemicalStack.EMPTY;
-				} else {
-					try {
-						yield readStack(new StringReader(str));
-					} catch (CommandSyntaxException ex) {
-						ex.printStackTrace();
-						yield ChemicalStack.EMPTY;
-					}
-				}
-			}
+	@HideFromJS
+	@Nullable
+	static ChemicalStack wrapStackTrivial(Context cx, Object from) {
+		while (from instanceof Wrapper w) {
+			from = w.unwrap();
+		}
+
+		var registries = RegistryAccessContainer.of(cx);
+
+		return switch (from) {
+			case null -> ChemicalStack.EMPTY;
+			case ChemicalStack id -> id;
+			case Chemical chem -> wrapStackTrivial(cx, wrapAsHolder(chem));
+			case Holder<Chemical> holder -> holder.is(MekanismAPI.EMPTY_CHEMICAL_KEY) ? ChemicalStack.EMPTY : new ChemicalStack(holder, FluidType.BUCKET_VOLUME);
+			default -> null;
 		};
 	}
 
-	static Holder<Chemical> read(StringReader reader) throws CommandSyntaxException {
-		reader.skipWhitespace();
-		return MekanismAPI.CHEMICAL_REGISTRY.getHolder(ResourceLocation.read(reader)).get();
+	@HideFromJS
+	static DataResult<ChemicalStack> tryWrapStack(Context cx, Object from) {
+		while (from instanceof Wrapper w) {
+			from = w.unwrap();
+		}
+
+		var registries = RegistryAccessContainer.of(cx);
+
+		var trivial = wrapStackTrivial(cx, from);
+		if (trivial != null) {
+			return DataResult.success(trivial);
+		}
+
+		// TODO: improve
+		var str = from.toString();
+
+		if (str.isEmpty() || str.equals("mekanism:empty")) {
+			return EMPTY_STACK_RESULT;
+		} else {
+			try {
+				return readStack(new StringReader(str));
+			} catch (CommandSyntaxException ex) {
+				return error(ex::getMessage);
+			}
+		}
 	}
 
-	static ChemicalStack readStack(StringReader reader) throws CommandSyntaxException {
-		reader.skipWhitespace();
-		var amount = FluidWrapper.readFluidAmount(reader);
-		var chemical = read(reader);
-		return new ChemicalStack(chemical, amount);
-	}
-
-	static ChemicalIngredient ingredientOf(Object from) {
+	@HideFromJS
+	@Nullable
+	static ChemicalIngredient wrapIngredientTrivial(Context cx, Object from) {
 		return switch (from) {
 			case null -> EmptyChemicalIngredient.INSTANCE;
 			case ChemicalIngredient c -> c;
 			case ChemicalStackIngredient c -> c.ingredient();
 			case ChemicalStack c -> new SingleChemicalIngredient(c.getChemicalHolder());
-			case Chemical c -> new SingleChemicalIngredient(c.getAsHolder());
+			case Chemical c -> new SingleChemicalIngredient(wrapAsHolder(c));
+			default -> null;
+		};
+	}
+
+	@HideFromJS
+	static DataResult<ChemicalIngredient> tryWrapIngredient(Context cx, Object from) {
+		if (from instanceof Wrapper w) {
+			from = w.unwrap();
+		}
+
+		var trivial = wrapIngredientTrivial(cx, from);
+		if (trivial != null) {
+			return DataResult.success(trivial);
+		}
+
+		return switch (from) {
 			case Iterable<?> itr -> {
-				var a = new ArrayList<ChemicalIngredient>();
+				var results = new ArrayList<ChemicalIngredient>();
 
-				for (var o : itr) {
-					var i = ingredientOf(o);
+				var failed = false;
+				Stream.Builder<String> errors = Stream.builder();
 
-					if (!i.isEmpty()) {
-						a.add(i);
+				for (var elem : itr) {
+					var ingredient = tryWrapIngredient(cx, elem);
+
+					ingredient.resultOrPartial()
+						.filter(ingr -> !ingr.isEmpty())
+						.ifPresent(results::add);
+
+					if (ingredient.isError()) {
+						failed = true;
+						errors.add(elem + ": " + ingredient.error().orElseThrow().message());
 					}
 				}
 
-				yield a.isEmpty() ? EmptyChemicalIngredient.INSTANCE : a.size() == 1 ? a.getFirst() : new CompoundChemicalIngredient(a);
-			}
-			default -> {
-				var str = from.toString();
-
-				if (str.isEmpty() || str.equals("mekanism:empty")) {
-					yield EmptyChemicalIngredient.INSTANCE;
+				if (failed) {
+					var msg = errors.build().collect(Collectors.joining("; "));
+					yield DataResult.error(() -> "Failed to parse chemical ingredient list: " + msg);
 				} else {
-					try {
-						yield readIngredient(new StringReader(str));
-					} catch (CommandSyntaxException ex) {
-						ex.printStackTrace();
-						yield EmptyChemicalIngredient.INSTANCE;
-					}
+					yield DataResult.success(switch (results.size()) {
+						case 0 -> EmptyChemicalIngredient.INSTANCE;
+						case 1 -> results.getFirst();
+						default -> new CompoundChemicalIngredient(results);
+					});
+				}
+			}
+			case CharSequence cs when cs.isEmpty() -> EMPTY_INGREDIENT_RESULT;
+			case CharSequence cs when cs.equals("mekanism:empty") -> EMPTY_INGREDIENT_RESULT;
+			default -> {
+				try {
+					yield readIngredient(cx, new StringReader(from.toString()));
+				} catch (CommandSyntaxException ex) {
+					yield error(ex::getMessage);
 				}
 			}
 		};
 	}
 
-	static ChemicalIngredient readIngredient(StringReader reader) throws CommandSyntaxException {
+	static ChemicalIngredient ingredientOf(Context cx, Object from) {
+		return tryWrapIngredient(cx, from)
+			.getOrThrow(error -> new KubeRuntimeException("Failed to read chemical ingredient from %s: %s".formatted(from, error))
+				.source(SourceLine.of(cx)));
+	}
+
+	@HideFromJS
+	static DataResult<ChemicalStackIngredient> tryWrapStackIngredient(Context cx, Object from) {
+		while (from instanceof Wrapper w) {
+			from = w.unwrap();
+		}
+
+		return switch (from) {
+			case null -> error(() -> "Chemical stack ingredient cannot be parsed from null!");
+			case ChemicalIngredient c when c.isEmpty() -> error(() -> "Cannot use empty ingredient for chemical stack ingredient!");
+			case ChemicalStack c when c.isEmpty() -> error(() -> "Cannot use empty chemical stack for chemical stack ingredient!");
+			case ChemicalStackIngredient c -> success(c);
+			case ChemicalIngredient c -> success(new ChemicalStackIngredient(c, FluidType.BUCKET_VOLUME));
+			case ChemicalStack c -> success(new ChemicalStackIngredient(
+				new SingleChemicalIngredient(c.getChemicalHolder()),
+				c.getAmount()
+			));
+			case Chemical c -> success(new ChemicalStackIngredient(
+				new SingleChemicalIngredient(wrapAsHolder(c)),
+				FluidType.BUCKET_VOLUME
+			));
+			default -> {
+				try {
+					yield readStackIngredient(new StringReader(from.toString()));
+				} catch (CommandSyntaxException ex) {
+					yield error(ex::getMessage);
+				}
+			}
+		};
+	}
+
+	static ChemicalStackIngredient stackIngredientOf(Context cx, Object from) {
+		return tryWrapStackIngredient(cx, from)
+			.getOrThrow(error -> new KubeRuntimeException("Failed to read chemical stack ingredient from %s: %s".formatted(from, error))
+				.source(SourceLine.of(cx)));
+	}
+
+	@HideFromJS
+	private static DataResult<Holder<Chemical>> find(ResourceLocation id) {
+		return MekanismAPI.CHEMICAL_REGISTRY
+			.getHolder(id)
+			.map(DataResult::success)
+			.orElseGet(() -> DataResult.error(() -> "Chemical with ID " + id + " does not exist!"))
+			.map(Function.identity());
+	}
+
+	static DataResult<Holder<Chemical>> read(StringReader reader) {
+		reader.skipWhitespace();
+
+		return ID.read(reader).flatMap(MekanismChemicalWrapper::find);
+	}
+
+	static DataResult<ChemicalStack> readStack(StringReader reader) throws CommandSyntaxException {
+		reader.skipWhitespace();
+
+		var amount = readChemicalAmount(reader);
+		var chemical = read(reader);
+
+		return chemical.apply2(ChemicalStack::new, amount);
+	}
+
+	static DataResult<ChemicalIngredient> readIngredient(Context cx, StringReader reader) throws CommandSyntaxException {
 		reader.skipWhitespace();
 
 		if (reader.peek() == '#') {
 			reader.skip();
-			return new TagChemicalIngredient(TagKey.create(MekanismAPI.CHEMICAL_REGISTRY_NAME, ResourceLocation.read(reader)));
+
+			return ID.read(reader)
+				.map(id -> TagKey.create(MekanismAPI.CHEMICAL_REGISTRY_NAME, id))
+				.map(TagChemicalIngredient::new);
 		}
 
-		return new SingleChemicalIngredient(read(reader));
+		return read(reader).map(SingleChemicalIngredient::new);
 	}
+
+	static DataResult<ChemicalStackIngredient> readStackIngredient(StringReader reader) throws CommandSyntaxException {
+		reader.skipWhitespace();
+
+		var amount = readChemicalAmount(reader);
+		var ingredient = readIngredient(null, reader);
+
+		return ingredient.apply2(ChemicalStackIngredient::new, amount);
+	}
+
 
 	@HideFromJS
-	static ChemicalStackIngredient stackIngredientOf(Object from) {
-		return switch (from) {
-			case null -> new ChemicalStackIngredient(EmptyChemicalIngredient.INSTANCE, 1);
-			case ChemicalStackIngredient c -> c;
-			case ChemicalIngredient c -> new ChemicalStackIngredient(c, FluidType.BUCKET_VOLUME);
-			case ChemicalStack c -> new ChemicalStackIngredient(new SingleChemicalIngredient(c.getChemicalHolder()), c.getAmount());
-			case Chemical c -> new ChemicalStackIngredient(new SingleChemicalIngredient(c.getAsHolder()), FluidType.BUCKET_VOLUME);
-			default -> {
-				var str = from.toString();
+	static DataResult<Long> readChemicalAmount(StringReader reader) throws CommandSyntaxException {
+		reader.skipWhitespace();
 
-				try {
-					yield readStackIngredient(new StringReader(str));
-				} catch (CommandSyntaxException ex) {
-					ex.printStackTrace();
-					yield new ChemicalStackIngredient(EmptyChemicalIngredient.INSTANCE, 1);
-				}
+		if (reader.canRead() && StringReader.isAllowedNumber(reader.peek())) {
+			var amountd = reader.readDouble();
+			reader.skipWhitespace();
+
+			if (reader.peek() == 'b' || reader.peek() == 'B') {
+				reader.skip();
+				reader.skipWhitespace();
+				amountd *= FluidType.BUCKET_VOLUME;
 			}
-		};
-	}
 
-	static ChemicalStackIngredient readStackIngredient(StringReader reader) throws CommandSyntaxException {
-		reader.skipWhitespace();
-		var amount = FluidWrapper.readFluidAmount(reader);
-		reader.skipWhitespace();
-		var ingredient = readIngredient(reader);
-		return new ChemicalStackIngredient(ingredient, amount);
+			if (reader.peek() == '/') {
+				reader.skip();
+				reader.skipWhitespace();
+				amountd = amountd / reader.readDouble();
+			}
+
+			var amount = (long) amountd;
+			reader.expect('x');
+			reader.skipWhitespace();
+
+			if (amount < 1) {
+				return error(() -> "Fluid amount smaller than 1 is not allowed!");
+			}
+
+			return success(amount);
+		}
+
+		return success((long) FluidType.BUCKET_VOLUME);
 	}
 }
